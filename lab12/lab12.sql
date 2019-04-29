b7ae8384bada7aa9722c21cbd148a9c0df3854cb
uncooleben <pengjuntao2016@foxmail.com>
2019-04-29 16:41:13 -0700

    lab13 added

--- lab12/lab12.sql
+++ lab12/lab12.sql
@@ -2,12 +2,22 @@
 
 -- Q2
 CREATE TABLE obedience AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT a.seven, b.animal
+  FROM students AS a, students AS b
+  WHERE a.time = b.time;
 
 -- Q3
 CREATE TABLE smallest_int AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT a.time, b.smallest
+  FROM students AS a, students AS b
+  WHERE a.time = b.time and b.smallest > 2
+  ORDER BY b.smallest ASC
+  LIMIT 20;
 
 -- Q4
 CREATE TABLE matchmaker AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT a.pet, a.song, a.color, b.color
+  FROM students AS a, students AS b
+  WHERE a.pet = b.pet and
+        a.song = b.song and
+	a.time < b.time;
